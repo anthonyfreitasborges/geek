3ac0cd5b42219fc8d189dc7219e8702593ec49ef
anthonyfreitasborges <103280259+anthonyfreitasborges@users.noreply.github.com>
2024-05-24 18:53:17 -0300

    Add files via upload

---
+++ GEEK/questions.js
@@ -0,0 +1,192 @@
+export default [
+    {
+      question: "Quem é o criador de One Piece?",
+      answers: [
+        { option: "Eiichirô Oda", correct: true },
+        { option: "Koyoharu Gotouge", correct: false },
+        { option: "Akira Toriyama", correct: false },
+        { option: "Masashi Kishimoto", correct: false },
+      ],
+    },
+    {
+      question: "Qual era o único jeito de derrotar a Kaguya ?",
+         answers: [
+        { option: "Usando as Técnicas de Ashura e Indra", correct: false },
+        { option: "Perfurando o coração", correct: false },
+        { option: "Selando", correct: true },
+        { option: "Juntando os Chakras das Bijuus", correct: false },
+      ],
+    },
+    {
+      question: "Onde Naruto aprendeu o modo sabio ?",
+          answers: [
+        { option: "Com Jiraya no País da Água", correct: false },
+        { option: "Em um Treinamento com Gamabunta", correct: false },
+        { option: "Com Minato", correct: false },
+        { option: "Monte Myoboku", correct: true },
+      ],
+    },
+    {
+      question: "Qual era o nome do primeiro vilão de Capitão América?",
+      answers: [
+        { option: "Soldado Invernal", correct: false },
+        { option: "Hulk Vermelho", correct: false },
+        { option: "Ronan O Acusador", correct: false },
+        { option: "Caveira Vermelha", correct: true },
+      ],
+    },
+    {
+        question: "Qual é o nome do pai de Peter Quill",
+        answers: [
+          { option: "Yondu", correct: false },
+          { option: "Thanos", correct: false },
+          { option: "Steve Rogers", correct: false },
+          { option: "Ego, o planeta vivo", correct: true },
+        ],
+      },
+      {
+        question: "Qual desses vingadores não foi dizimado pelo Estralar de dedo do Thanos?",
+        answers: [
+          { option: "Vespa", correct: false },
+          { option: "Homem Aranha", correct: false },
+          { option: "Homem Formiga", correct: true },
+          { option: "Pantera Negra", correct: false },
+        ],
+      },
+      {
+        question: "Qual os dois nomes verdadeiros do Superman, de humano e de alíenigina, respectivamente?",
+        answers: [
+          { option: "Clark Kent e Kal-El", correct: true },
+          { option: "Kal-El e Bruce Wayne", correct: false },
+          { option: "Klark Cant e Krytoniano", correct: false },
+          { option: "Bruce Wayne e Mon-el", correct: false },
+        ],
+      },
+      {
+        question: "O que o Superman é da supergirl?",
+        answers: [
+          { option: "Apenas Amigos", correct: false },
+          { option: "Primos", correct: true },
+          { option: "Irmãos", correct: false },
+          { option: "Namorados", correct: false },
+        ],
+      },
+      {
+        question: "Qual nome do espadachim do chapéus de palha?",
+        answers: [
+          { option: "Sanji", correct: false },
+          { option: "Usopp", correct: false },
+          { option: "Luffy", correct: false },
+          { option: "Zoro", correct: true },
+        ],
+      },
+      {
+        question: "Quem treinou o Luffy?",
+        answers: [
+          { option: "Shanks", correct: false },
+          { option: "Rayleigh", correct: true },
+          { option: "Law", correct: false },
+          { option: "Gol D. Roger", correct: false },
+        ],
+      },
+       {
+        question: "Quem é o pai do Baki Hanma?",
+        answers: [
+          { option: "Yujiro Hanma", correct: true },
+          { option: "Yuchiro Hanma", correct: false },
+          { option: "Jack Hanma", correct: false },
+          { option: "Retsu Kaioh", correct: false },
+        ],
+      },
+
+       {
+        question: "Quem é o humano das cavernas em Baki?",
+        answers: [
+          { option: "Doppo", correct: false },
+          { option: "Oliver", correct: false },
+          { option: "Pickle", correct:  true },
+          { option: "Hanayama", correct: false },
+        ],
+      },
+
+{
+        question: "Qual nome da mãe do Baki?",
+        answers: [
+          { option: "Natsui Orochi", correct: false },
+          { option: "Diane Neil", correct: false },
+          { option: "Kozue Matsumoto", correct:  false},
+          { option: "Emi Akezawa", correct: true },
+        ],
+      },
+
+
+    {
+        question: "Quem é o protagonita e antagonista de Berseker?",
+        answers: [
+          { option: "Guts e Griffth", correct: true },
+          { option: "Femto e Gambino  ", correct: false },
+          { option: "Guts e Zodd", correct:  false},
+          { option: "Femto e Donavam", correct: false},
+        ],
+      },
+{
+        question: "Qual o shikigami mais forte invocado por Megumi Fushiguro?",
+        answers: [
+          { option: "Nue", correct: false },
+          { option: "Orochi", correct: false },
+          { option: "Gama", correct:  false},
+          { option: "General Mahoraga", correct: true },
+        ],
+      },
+
+    {
+        question: "Quem é o atual hospedeiro do Sukuna?"  ,
+        answers: [
+          { option: "Yuji Itadori", correct: false },
+          { option: "Satoru Gojo", correct: false },
+          { option: "Megumi Fushiguro", correct:  true},
+          { option: "Kenjaku", correct: false},
+        ],
+      },
+
+  {
+        question: "Qual o protagonista de De volta para o futuro?"  ,
+        answers: [
+          { option: "George McFly", correct: false },
+          { option: "Marty McFly", correct: true },
+          { option: "Biff Tannen", correct:  false},
+          { option: "Match ", correct: false},
+        ],
+      },
+{
+        question: "Quem é o pai de Luke Skywalker?"  ,
+        answers: [
+          { option: "Obi-Wan", correct: false },
+          { option: "Yoda", correct: false },
+          { option: "Darth Vader", correct:  true},
+          { option: "Han Solo ", correct: false},
+        ],
+      },
+{
+        question: "Qual casa o chapéu seletor mandou Harry Potter?"  ,
+        answers: [
+          { option: "Sonserina", correct: false },
+          { option: "Corvinal", correct: false },
+          { option: "Lufa-Lufa", correct:  false},
+          { option: "Grifinória", correct: true},
+        ],
+      },
+{
+        question: "Qual o nome do mestre de Daniel Larusso em Karatê Kid?"  ,
+        answers: [
+          { option: "Nariyoshi Miyagi", correct: false },
+          { option: "Chozen Miyagi", correct: false },
+          { option: "Nakamura Miyagi", correct:  false},
+          { option: "Johnny Lawrence", correct: true},
+        ],
+      },
+
+    
+  ];
+
+
